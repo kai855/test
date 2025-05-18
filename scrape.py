f12800dbfae284fe2813dc4e48464ea4fe5b17bb
kai855 <54061535+kai855@users.noreply.github.com>
2025-05-18 13:43:59 +0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -2,7 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-url = "https://mly1.543412546.xyz/api/v1/client/subscribe?token=615c69d35a156a4e94b0c5f9e77c811d"
+pages = {
+    "page1": "https://mly1.543412546.xyz/api/v1/client/subscribe?token=615c69d35a156a4e94b0c5f9e77c811d ",
+    "page2": "https://xtls.rprx.vision/api/v1/client/subscribe?token=02034b23af5a70f14815ab3a7ea0159f"
+}
+
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0 Safari/537.36"
 }
@@ -17,4 +22,13 @@ with open(filename, "w", encoding="utf-8") as f:
     f.write("Page content:\n")
     f.write(response.text)  
 
-print(f"已保存为 {filename}")
+
+for name, url in pages.items():
+    print(f"正在抓取: {url}")
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0 Safari/537.36"
+}
+    response = requests.get(url, headers=headers, timeout=10, verify=False)
+    if response.status_code == 200:
+        with open(f"{name}.txt", "w", encoding="utf-8") as f:
+            f.write(response.text)
