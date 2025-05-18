f0aadd21726b4950d750381c2fb547c17c3fb928
kai855 <54061535+kai855@users.noreply.github.com>
2025-05-18 13:22:48 +0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -2,11 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-url = "https://baidu.com"
+url = "https://mly1.543412546.xyz/api/v1/client/subscribe?token=615c69d35a156a4e94b0c5f9e77c811d"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0 Safari/537.36"
 }
-response = requests.get(url, headers=headers, timeout=10)
+response = requests.get(url, headers=headers, timeout=10, verify=False)
 soup = BeautifulSoup(response.text, "html.parser")
 title = soup.title.string if soup.title else "No Title"
 
