f85b511fcc5bfb6f350295c031231e4073ee79f3
kai855 <54061535+kai855@users.noreply.github.com>
2025-05-18 13:16:58 +0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-url = "https://example.com "
+url = "https://baidu.com "
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 title = soup.title.string if soup.title else "No Title"
